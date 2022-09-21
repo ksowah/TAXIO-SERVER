6b208bfc5a2735799f227b0588553a473af6d508
ksowah <sowahkelvin640@gmail.com>
2022-09-21 20:07:22 +0000

    query active session

---
+++ src/modules/user/me.ts
@@ -0,0 +1,26 @@
+import { Resolver, Query, Ctx } from "type-graphql";
+import User, { Person } from "../../models/UserModel"
+import { MyContext } from "src/types/myContext";
+
+// fix the error userId is not a property of session
+declare module 'express-session' {
+    export interface SessionData {
+      userId: any;
+    }
+  }
+
+@Resolver()
+export class MeResolver {
+  // its okay to leave a dummy querry to make the resolver work
+  @Query(() => Person, { nullable: true })
+  async me(@Ctx() ctx: MyContext) : Promise<Person | null> {
+
+    if(!ctx.req.session!.userId) {
+      return null;
+    }
+
+    const user = await User.findById(ctx.req.session.userId);
+    
+    return user;
+  }
+}
