e3f4e6f97f359b0c092648fc9a44efcc376ae7d4
ksowah <sowahkelvin640@gmail.com>
2022-09-22 20:09:20 +0000

    confirmation url

---
+++ src/utils/confirmationUrl.ts
@@ -0,0 +1,11 @@
+import { v4 } from "uuid"
+import { redis } from "../redis"
+
+export const createConfirmationUrl = async (userId: string) => {
+    const token = v4()
+
+    // store userId with the token as a key in redis and set the expiration time to 1 day
+    await redis.set(token, userId, "EX", 60 * 60 * 24) // 1 day expiration
+
+    return `http://localhost:3000/user/confirm/${token}`
+}
