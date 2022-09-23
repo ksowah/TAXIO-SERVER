40b93ae92a1c3ef5e396260238229bb9ea6be672
ksowah <sowahkelvin640@gmail.com>
2022-09-23 23:11:09 +0000

    direct resolvers

--- src/index.ts
+++ src/index.ts
@@ -4,14 +4,9 @@ import * as Express from "express"
 import { buildSchema } from "type-graphql";
 import * as dotenv from "dotenv"
 import connectDB from "./config/db";
-import { RegisterResolver } from "./modules/user/registerUser";
 import session = require("express-session");
 import { sessionOption } from "./redis";
 import cors = require("cors");
-import { LoginResolver } from "./modules/user/login";
-import { MeResolver } from "./modules/user/me";
-import { ConfirmUserResolver } from "./modules/user/confirmUser";
-
 
 dotenv.config()
 
@@ -19,7 +14,8 @@ connectDB()
 
 const main = async () => {
     const schema = await buildSchema({
-        resolvers: [RegisterResolver, LoginResolver, MeResolver, ConfirmUserResolver],
+      // get all resolvers in directory
+      resolvers: [__dirname + "/modules/**/*.ts"],
         // authChecker: ({ context: { req } }) => {
         //   // returns  true if the user is logged in else false
         //     return !!req.session.userId
@@ -28,7 +24,7 @@ const main = async () => {
     
    const apolloServer = new ApolloServer({
     schema,
-    context: ({ req }) => ({ req }), // pass the express request to the context to be used in the resolvers 
+    context: ({ req, res }) => ({ req, res }), // pass the express request to the context to be used in the resolvers 
 })
 
     const app = Express();
