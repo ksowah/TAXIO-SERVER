55fcdf35ea36e0b6c4f5acdf78e11499062d4743
ksowah <sowahkelvin640@gmail.com>
2022-09-24 23:14:16 +0000

    add prefix

--- src/utils/confirmationUrl.ts
+++ src/utils/confirmationUrl.ts
@@ -1,3 +1,4 @@
+import { confirmationPrefix } from "../constatnts/prefixes";
 import { v4 } from "uuid"
 import { redis } from "../redis"
 
@@ -5,7 +6,7 @@ export const createConfirmationUrl = async (userId: string) => {
     const token = v4()
 
     // store userId with the token as a key in redis and set the expiration time to 1 day
-    await redis.set(token, userId, "EX", 60 * 60 * 24) // 1 day expiration
+    await redis.set(confirmationPrefix + token, userId, "EX", 60 * 60 * 24) // 1 day expiration
 
     return `http://localhost:3000/user/confirm/${token}`
 }
