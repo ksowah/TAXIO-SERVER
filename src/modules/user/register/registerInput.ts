917db86a8b9ac25448d181ba8067aa2e088aff21
ksowah <sowahkelvin640@gmail.com>
2022-09-24 23:13:39 +0000

    extend inputs

--- src/modules/user/register/registerInput.ts
+++ src/modules/user/register/registerInput.ts
@@ -1,10 +1,11 @@
 import { IsEmail, Length } from "class-validator";
+import { PasswordInput } from "../../../shared/passwordInput";
 import { Field, InputType } from "type-graphql";
 import { IsEmailAlreadyExist } from "./isEmailAlreadyUsed";
 
 
 @InputType()
-export class RegisterInput {
+export class RegisterInput extends PasswordInput {
     
     @Field() 
     @Length(1, 255, {message: "First name must be more than 1 character"})  
@@ -18,9 +19,5 @@ export class RegisterInput {
     @IsEmail({message: "email must be a valid email"})
     @IsEmailAlreadyExist({message: "A user already exists with this email"})
     email: string
-
-    @Field() 
-    @Length(6, 255, {message: "Password must be more than 6 characters"})
-    password: string
   
 }
