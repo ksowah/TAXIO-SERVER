b154bff66042be5207d80f62a64fa52a6079eb2c
ksowah <sowahkelvin640@gmail.com>
2022-09-27 23:08:07 +0000

    update input type

--- src/modules/user/register/registerInput.ts
+++ src/modules/user/register/registerInput.ts
@@ -1,4 +1,4 @@
-import { IsEmail, Length } from "class-validator";
+import { IsEmail } from "class-validator";
 import { PasswordInput } from "../../../shared/passwordInput";
 import { Field, InputType } from "type-graphql";
 import { IsEmailAlreadyExist } from "./isEmailAlreadyUsed";
@@ -6,14 +6,6 @@ import { IsEmailAlreadyExist } from "./isEmailAlreadyUsed";
 
 @InputType()
 export class RegisterInput extends PasswordInput {
-    
-    @Field() 
-    @Length(1, 255, {message: "First name must be more than 1 character"})  
-    firstName: string
-
-    @Field() 
-    @Length(1, 255, {message: "Last name must be more than 1 character"})
-    lastName: string
 
     @Field() 
     @IsEmail({message: "email must be a valid email"})
