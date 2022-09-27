9d398f3c4e1903ff1153f0e050ba3af11475392e
ksowah <sowahkelvin640@gmail.com>
2022-09-27 15:24:00 +0000

    set up index

--- src/index.ts
+++ src/index.ts
@@ -4,9 +4,8 @@ import * as Express from "express"
 import { buildSchema } from "type-graphql";
 import * as dotenv from "dotenv"
 import connectDB from "./config/db";
-import session = require("express-session");
-import { sessionOption } from "./redis";
 import cors = require("cors");
+
 
 dotenv.config()
 
@@ -16,10 +15,6 @@ const main = async () => {
     const schema = await buildSchema({
       // get all resolvers in directory
       resolvers: [__dirname + "/modules/**/*.ts"],
-        // authChecker: ({ context: { req } }) => {
-        //   // returns  true if the user is logged in else false
-        //     return !!req.session.userId
-        // }
     })
     
    const apolloServer = new ApolloServer({
@@ -36,8 +31,6 @@ const main = async () => {
       credentials: true,
     };
     app.use(cors(corsOptions))
-    
-      app.use(session(sessionOption));
 
     apolloServer.applyMiddleware({ app, cors: corsOptions });
     
