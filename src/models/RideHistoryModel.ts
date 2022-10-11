ca273d2c25113aadb2f6b636312b99bd50ffdf04
ksowah <sowahkelvin640@gmail.com>
2022-10-11 21:44:21 +0000

    unique fiels for ride history

--- src/models/RideHistoryModel.ts
+++ src/models/RideHistoryModel.ts
@@ -3,7 +3,8 @@ import mongoose from "mongoose";
 const RideHistorySchema = new mongoose.Schema({
     description: {
         type: String,
-        required: true
+        required: true,
+        unique: true
     },
     
     lat: {
